d8dc65f4ec0acf13397c1419b0e7c4a62c9544b5
unknown <baldee229@gmail.com>
2022-04-06 14:39:07 +0300

    try3000

---
+++ app.js
@@ -0,0 +1,19 @@
+let str1 = "BAB";
+let str2 = "door";
+let str3 = "sas";
+
+
+function ifpland(string) {
+
+    for (let i = 0; i < string.length; i++) {
+
+        if (string[i] !== string[string.length - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log("str1: "+ifpland(str1));
+console.log("str2: "+ifpland(str2));
+console.log("str3: "+ifpland(str3));//trtytr
